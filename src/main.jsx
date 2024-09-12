afd7bfb7e5b457321afbc88977619f7d602227e0
Anil Unni <anil.unni@ociuz.in>
2024-09-12 10:55:51 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useRef, useState} from "react";
-import { createRoot } from 'react-dom/client'
+import React, { useEffect, useRef, useState } from "react";
+import { createRoot } from 'react-dom/client';
 import * as THREE from 'three';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 const style = {
     height: 500 // we can control scene size by setting container dimensions
 };
 
-const App = props => {
+const App = ({ colors, onMeshUpdate, onProgress, elephColor, addEleph, removeEleph }) => {
     const thisRef = useRef({});
     const mountRef = useRef(null);
 
@@ -22,14 +22,45 @@ const App = props => {
             window.removeEventListener('resize', thisRef.current.handleWindowResize);
             window.cancelAnimationFrame(thisRef.current.requestID);
             thisRef.current.controls.dispose();
-        }
+        };
     }, []);
 
+    useEffect(() => {
+        if (thisRef.current.model) {
+            thisRef.current.model.traverse(child => {
+                if (child instanceof THREE.Mesh) {
+                    const colorIndex = thisRef.current.meshes.indexOf(child);
+                    if (colorIndex !== -1 && colors[colorIndex]) {
+                        child.material.color.setHex(colors[colorIndex]); // Apply the specific color
+                    }
+                }
+            });
+        }
+    }, [colors]);
 
-    // Standard scene setup in Three.js. Check "Creating a scene" manual for more information
-    // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+    useEffect(() => {
+        if (thisRef.current.eleph) {
+            thisRef.current.eleph.traverse(child => {
+                if (child instanceof THREE.Mesh) {
+                    child.material.color.setHex(elephColor); // Update the color of the added model
+                }
+            });
+        }
+    }, [elephColor]);
+
+    useEffect(() => {
+        if (addEleph) {
+            thisRef.current.addEleph();
+        }
+    }, [addEleph]);
+
+    useEffect(() => {
+        if (removeEleph) {
+            thisRef.current.removeEleph();
+        }
+    }, [removeEleph]);
+
     thisRef.current.sceneSetup = () => {
-        // get container dimensions and use them for scene sizing
         const width = mountRef.current.clientWidth;
         const height = mountRef.current.clientHeight;
 
@@ -40,96 +71,110 @@ const App = props => {
             0.1, // near plane
             1000 // far plane
         );
-        thisRef.current.camera.position.z = 500; // is used here to set some distance from a cube that is located at z = 0
-        // OrbitControls allow a camera to orbit around the object
-        // https://threejs.org/docs/#examples/controls/OrbitControls
-        thisRef.current.controls = new OrbitControls( thisRef.current.camera, mountRef.current );
+        thisRef.current.camera.position.z = 25;
+        thisRef.current.controls = new OrbitControls(thisRef.current.camera, mountRef.current);
         thisRef.current.renderer = new THREE.WebGLRenderer();
-        thisRef.current.renderer.setSize( width, height );
-        mountRef.current.appendChild( thisRef.current.renderer.domElement ); // mount using React ref
+        thisRef.current.renderer.setSize(width, height);
+        mountRef.current.appendChild(thisRef.current.renderer.domElement);
     };
 
-    // Code below is taken from Three.js OBJ Loader example
-    // https://threejs.org/docs/#examples/en/loaders/OBJLoader
     thisRef.current.loadTheModel = () => {
-        // instantiate a loader
         const loader = new OBJLoader();
-
-        // load a resource
         loader.load(
-            // resource URL relative to the /public/index.html of the app
-            'eleph.obj',
-            // called when resource is loaded
-            ( object ) => {
-                thisRef.current.scene.add( object );
+            'new.obj',
+            (object) => {
+                console.log("Model loaded:", object);
+                const meshes = [];
+                object.traverse(child => {
+                    if (child instanceof THREE.Mesh) {
+                        child.material = new THREE.MeshStandardMaterial({ color: 0xffffff }); // Default white
+                        meshes.push(child); // Collect all meshes
+                    }
+                })
+                console.log("Meshes:", meshes);
+                thisRef.current.model = object;
+                thisRef.current.meshes = meshes;
 
-                // get the newly added object by name specified in the OBJ model (that is Elephant_4 in my case)
-                // you can always set console.log(this.scene) and check its children to know the name of a model
-                const el = thisRef.current.scene.getObjectByName("Elephant_4");
+                // Initialize colors for each mesh
+                thisRef.current.meshes.forEach((mesh, index) => {
+                    mesh.material.color.setHex(colors[index] || 0xffffff); // Set default color
+                });
 
-                // change some custom props of the element: placement, color, rotation, anything that should be
-                // done once the model was loaded and ready for display
-                el.material.color.set(0x50C878);
-                el.rotation.x = 23.5;
-
-                // centering the model
-                // THREE.Box3() is AABB (axis-aligned bounding box). You can set it from the object you've loaded.
-                // Then use .getCenter() method to get its center. Then simply subtract the vector of the center
-                // from the default position of the object. https://threejs.org/docs/index.html#api/en/math/Box3
-                const box = new THREE.Box3().setFromObject( el );
+                thisRef.current.scene.add(object);
+                const box = new THREE.Box3().setFromObject(object);
                 const center = new THREE.Vector3();
-                box.getCenter( center );
-                el.position.sub( center );
+                box.getCenter(center);
+                object.position.sub(center);
 
-                // make this element available inside of the whole component to do any animation later
-                thisRef.current.model = el;
+                // Notify parent of loaded meshes
+                onMeshUpdate(thisRef.current.meshes);
             },
-            // called when loading is in progresses
-            ( xhr ) => {
-
-                const loadingPercentage = Math.ceil(xhr.loaded / xhr.total * 100);
-                console.log( ( loadingPercentage ) + '% loaded' );
-
-                // update parent react component to display loading percentage
-                props.onProgress(loadingPercentage);
+            (xhr) => {
+                const loadingPercentage = Math.ceil((xhr.loaded / xhr.total) * 100);
+                onProgress(loadingPercentage);
             },
-            // called when loading has errors
-            ( error ) => {
-
-                console.log( 'An error happened:' + error );
-
+            (error) => {
+                console.log('An error happened:' + error);
             }
         );
     };
 
-    // adding some lights to the scene
+    thisRef.current.addEleph = () => {
+        const loader = new OBJLoader();
+        loader.load(
+            'eleph.obj',
+            (object) => {
+                console.log("Eleph Model loaded:", object);
+                object.scale.set(0.01, 0.01, 0.01);
+                object.traverse(child => {
+                    if (child instanceof THREE.Mesh) {
+                        child.material = new THREE.MeshStandardMaterial({ color: elephColor });
+                    }
+                });
+                thisRef.current.eleph = object;
+                thisRef.current.scene.add(object);
+
+                // Center the object
+                const box = new THREE.Box3().setFromObject(object);
+                const center = new THREE.Vector3();
+                box.getCenter(center);
+                object.position.sub(center);
+            },
+            (xhr) => {
+                const loadingPercentage = Math.ceil((xhr.loaded / xhr.total) * 100);
+                onProgress(loadingPercentage);
+            },
+            (error) => {
+                console.log('An error happened:' + error);
+            }
+        );
+    };
+
+    thisRef.current.removeEleph = () => {
+        if (thisRef.current.eleph) {
+            thisRef.current.scene.remove(thisRef.current.eleph);
+            thisRef.current.eleph = null;
+        }
+    };
+
     thisRef.current.addLights = () => {
         const lights = [];
 
-        // set color and intensity of lights
-        lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-        lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-        lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+        lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+        lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+        lights[2] = new THREE.PointLight(0xffffff, 1, 0);
 
-        // place some lights around the scene for best looks and feel
-        lights[ 0 ].position.set( 0, 2000, 0 );
-        lights[ 1 ].position.set( 1000, 2000, 1000 );
-        lights[ 2 ].position.set( - 1000, - 2000, - 1000 );
+        lights[0].position.set(0, 2000, 0);
+        lights[1].position.set(1000, 2000, 1000);
+        lights[2].position.set(-1000, -2000, -1000);
 
-        thisRef.current.scene.add( lights[ 0 ] );
-        thisRef.current.scene.add( lights[ 1 ] );
-        thisRef.current.scene.add( lights[ 2 ] );
+        thisRef.current.scene.add(lights[0]);
+        thisRef.current.scene.add(lights[1]);
+        thisRef.current.scene.add(lights[2]);
     };
 
     thisRef.current.startAnimationLoop = () => {
-        // slowly rotate an object
-        if (thisRef.current.model) thisRef.current.model.rotation.z += 0.005;
-
-        thisRef.current.renderer.render( thisRef.current.scene, thisRef.current.camera );
-
-        // The window.requestAnimationFrame() method tells the browser that you wish to perform
-        // an animation and requests that the browser call a specified function
-        // to update an animation before the next repaint
+        thisRef.current.renderer.render(thisRef.current.scene, thisRef.current.camera);
         thisRef.current.requestID = window.requestAnimationFrame(thisRef.current.startAnimationLoop);
     };
 
@@ -137,33 +182,92 @@ const App = props => {
         const width = mountRef.current.clientWidth;
         const height = mountRef.current.clientHeight;
 
-        thisRef.current.renderer.setSize( width, height );
+        thisRef.current.renderer.setSize(width, height);
         thisRef.current.camera.aspect = width / height;
-
-        // Note that after making changes to most of camera properties you have to call
-        // .updateProjectionMatrix for the changes to take effect.
         thisRef.current.camera.updateProjectionMatrix();
     };
 
     return <div style={style} ref={mountRef} />;
-}
+};
 
 const Container = () => {
     const [state, setState] = useState({ isMounted: true });
+    const [colors, setColors] = useState([]);
+    const [elephColor, setElephColor] = useState(0xffffff); // Default color for added model
+    const [meshes, setMeshes] = useState([]);
+    const [loadingPercentage, setLoadingPercentage] = useState(0);
+    const [addEleph, setAddEleph] = useState(false);
+    const [removeEleph, setRemoveEleph] = useState(false);
 
-    const {isMounted = true, loadingPercentage = 0} = state;
+    const { isMounted } = state;
+
+    const handleColorChange = (index) => (e) => {
+        const newColor = e.target.value;
+        setColors(prevColors => {
+            const updatedColors = [...prevColors];
+            updatedColors[index] = parseInt(newColor.replace("#", "0x"), 16);
+            return updatedColors;
+        });
+    };
+
+    const handleElephColorChange = (e) => {
+        const newColor = e.target.value;
+        setElephColor(parseInt(newColor.replace("#", "0x"), 16));
+    };
+
     return (
         <>
-            <button onClick={() => setState(state => ({ ...state, isMounted: !state.isMounted}))}>
+            <button onClick={() => setState({ ...state, isMounted: !isMounted })}>
                 {isMounted ? "Unmount" : "Mount"}
             </button>
-            {isMounted && <App onProgress={loadingPercentage => setState(state => ({ ...state, loadingPercentage }))} />}
-            {isMounted && loadingPercentage === 100 && <div>Scroll to zoom, drag to rotate</div>}
-            {isMounted && loadingPercentage !== 100 && <div>Loading Model: {loadingPercentage}%</div>}
+            <button onClick={() => { setAddEleph(true); setRemoveEleph(false); }}>
+                Add Elephant
+            </button>
+            <button onClick={() => { setAddEleph(false); setRemoveEleph(true); }}>
+                Remove Elephant
+            </button>
+            {isMounted && (
+                <App
+                    colors={colors}
+                    elephColor={elephColor}
+                    onMeshUpdate={setMeshes}
+                    onProgress={setLoadingPercentage}
+                    addEleph={addEleph}
+                    removeEleph={removeEleph}
+                />
+            )}
+            {isMounted && loadingPercentage === 100 && (
+                <>
+                    <div>
+                        Scroll to zoom, drag to rotate
+                    </div>
+                    {meshes.map((mesh, index) => (
+                        <div key={index}>
+                            <label>
+                                Object {index + 1} Color:
+                                <input
+                                    type="color"
+                                    value={colors[index] ? `#${colors[index].toString(16).padStart(6, '0')}` : '#ffffff'}
+                                    onChange={handleColorChange(index)}
+                                />
+                            </label>
+                        </div>
+                    ))}
+                    <div>
+                        <label>
+                            Elephant Color:
+                            <input
+                                type="color"
+                                value={`#${elephColor.toString(16).padStart(6, '0')}`}
+                                onChange={handleElephColorChange}
+                            />
+                        </label>
+                    </div>
+                </>
+            )}
         </>
-    )
-}
+    );
+};
 
-createRoot(document.getElementById('root')).render(
-    <Container />
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<Container />);
